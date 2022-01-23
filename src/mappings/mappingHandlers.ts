7909f5f7a6265863220f006d4c1cc1f46e7a0e34
Swiss Lertpattanasak <swiss@muze.co.th>
2022-01-23 23:27:16 +0700

    [+] Exercise 1: Index Staking Rewards

---
+++ src/mappings/mappingHandlers.ts
@@ -0,0 +1,13 @@
+import { Balance } from '@polkadot/types/interfaces'
+import { SubstrateEvent } from '@subql/types'
+import { StakingReward } from '../types'
+
+export async function handleStakingRewarded(event: SubstrateEvent): Promise<void> {
+    const {event: {data: [account, newReward]}} = event;
+    const entity = new StakingReward(`${event.block.block.header.number}-${event.idx.toString()}`);
+    entity.account = account.toString();
+    entity.balance = (newReward as Balance).toBigInt();
+    entity.date = event.block.timestamp;
+    entity.blockHeight = event.block.block.header.number.toNumber();
+    await entity.save();
+}
