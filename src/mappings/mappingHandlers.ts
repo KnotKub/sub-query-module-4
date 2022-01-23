694328801e1287f6b26d240f8f3df0a9d7f054d5
Swiss Lertpattanasak <swiss@muze.co.th>
2022-01-24 00:06:48 +0700

    [+] Exercise 4: Reward v Rewarded (in this project i already fixed block height variable name effect to query in graphQL)

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -40,3 +40,10 @@ export async function handleSumRewarded(event: SubstrateEvent): Promise<void> {
     entity.blockHeight = event.block.block.header.number.toNumber();
     await entity.save();
 }
+
+export async function handleSumReward(event: SubstrateEvent): Promise<void> {
+    await handleSumRewarded(event)
+}
+export async function handleStakingReward(event: SubstrateEvent): Promise<void> {
+    await handleStakingRewarded(event)
+}
