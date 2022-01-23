982aab61a7ca41c9e064f1ad55e048a2e36628d1
Swiss Lertpattanasak <swiss@muze.co.th>
2022-01-23 23:48:21 +0700

    [+] Exercise 2: Aggregate Staking Rewards

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,6 +1,6 @@
 import { Balance } from '@polkadot/types/interfaces'
 import { SubstrateEvent } from '@subql/types'
-import { StakingReward } from '../types'
+import { StakingReward, SumReward } from '../types'
 
 export async function handleStakingRewarded(event: SubstrateEvent): Promise<void> {
     const {event: {data: [account, newReward]}} = event;
@@ -8,6 +8,35 @@ export async function handleStakingRewarded(event: SubstrateEvent): Promise<void
     entity.account = account.toString();
     entity.balance = (newReward as Balance).toBigInt();
     entity.date = event.block.timestamp;
+    await entity.save();
+}
+
+function createSumReward(accountId: string): SumReward {
+    const entity = new SumReward(accountId);
+    entity.totalReward = BigInt(0);
+    return entity;
+}
+
+/**
+ * example query
+ *  query {
+        sumRewards(first:3 orderBy:BLOCK_HEIGHT_ASC) {
+            nodes {
+                blockHeight
+                id
+                totalReward
+            }
+        }
+    }
+ * @param event 
+ */
+export async function handleSumRewarded(event: SubstrateEvent): Promise<void> {
+    const {event: {data: [account, newReward]}} = event;
+    let entity = await SumReward.get(account.toString());
+    if (entity === undefined){
+        entity = createSumReward(account.toString());
+    }
+    entity.totalReward = entity.totalReward + (newReward as Balance).toBigInt();
     entity.blockHeight = event.block.block.header.number.toNumber();
     await entity.save();
 }
